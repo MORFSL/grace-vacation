e06413964e49735a1d8b99242c87266e63053935
BlazeIsClone <sandevabeykoon123@gmail.com>
2025-10-16 16:22:25 +0530

    update: page 404 style

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -6,13 +6,13 @@ import { Button } from '@/components/ui/button'
 export default function NotFound() {
   return (
     <div className="container py-28">
-      <div className="prose max-w-none">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
+      <div className="prose max-w-none text-center">
+        <h1 className="mb-4 text-primary text-9xl">404</h1>
+        <p className="mb-6 font-medium">This page could not be found.</p>
+        <Button asChild variant="default" className="no-underline">
+          <Link href="/">Go home</Link>
+        </Button>
       </div>
-      <Button asChild variant="default">
-        <Link href="/">Go home</Link>
-      </Button>
     </div>
   )
 }
